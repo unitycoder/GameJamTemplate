3c16c87576cb4fdb858144e0366203b2b2c5e244
Mrdrra_Mint <srkyman@gmail.com>
2017-07-04 17:40:28 +0200

    CameraFollow2D added

---
+++ Assets/Scripts/Common/2D/CameraFollow2D.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum FollowType{
+	Strict,
+	Smooth
+}
+
+public class CameraFollow2D : MonoBehaviour {
+
+	public Transform leader;
+	public FollowType followType;
+
+	public float smoothness = 1f;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - leader.position;
+	}
+
+	void LateUpdate () {
+		updateCameraPosition (followType);
+	}
+
+	void updateCameraPosition(FollowType type){
+		switch (type) {
+		case FollowType.Strict:
+			transform.position = leader.position + offset;
+			break;
+		case FollowType.Smooth:
+			transform.position = Vector3.Lerp (transform.position, leader.position + offset, Time.deltaTime * smoothness);
+			break;
+		}
+	}
+}
